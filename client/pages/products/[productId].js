0d5a4725e252b4ce3dbc7e7287ede05ef1ca5b27
K_on_Mac <gemigpost@hotmail.com>
2020-11-28 19:12:29 +0100

    searchpath adjustment

--- client/pages/products/[productId].js
+++ client/pages/products/[productId].js
@@ -1,12 +1,12 @@
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
-const TicketShow = ({ ticket }) => {
+const ProductShow = ({ product }) => {
   const { doRequest, errors } = useRequest({
     url: '/api/orders',
     method: 'post',
     body: {
-      ticketId: ticket.id,
+      productId: product.id,
     },
     onSuccess: (order) =>
       Router.push('/orders/[orderId]', `/orders/${order.id}`),
@@ -14,8 +14,8 @@ const TicketShow = ({ ticket }) => {
 
   return (
     <div>
-      <h1>{ticket.title}</h1>
-      <h4>Price: {ticket.price}</h4>
+      <h1>{product.title}</h1>
+      <h4>Price: {product.price}</h4>
       {errors}
       <button onClick={() => doRequest()} className="btn btn-primary">
         Purchase
@@ -24,11 +24,11 @@ const TicketShow = ({ ticket }) => {
   );
 };
 
-TicketShow.getInitialProps = async (context, client) => {
-  const { ticketId } = context.query;
-  const { data } = await client.get(`/api/tickets/${ticketId}`);
+ProductShow.getInitialProps = async (context, client) => {
+  const { productId } = context.query;
+  const { data } = await client.get(`/api/products/${productId}`);
 
-  return { ticket: data };
+  return { product: data };
 };
 
-export default TicketShow;
+export default ProductShow;
